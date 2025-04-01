34b60704297c4fe4781190a87dd229b5f4ccf7c0
WrenF <m.feliza@nouvellesimages.xyz>
2025-04-01 16:59:42 +0200

    01_04_25_2DProjects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,17 @@ using UnityEngine.Serialization;
 public class PlayerController : MonoBehaviour
 {
     private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+    private static readonly int Jump = Animator.StringToHash("Jump");
+    private static readonly int Attack = Animator.StringToHash("Attack");
+    
     public float moveSpeed = 5f;
     [FormerlySerializedAs("JumpForce")] public float jumpForce = 20f;
+    public float attackDamage = 1f;
     
     private Rigidbody2D _rigidbody;
     private Animator _animator;
     private SpriteRenderer _spriteRenderer;
+    private bool isGrounded;
 
     void Start()
     {
@@ -22,16 +27,21 @@ public class PlayerController : MonoBehaviour
     {
         bool isJumping = Input.GetButtonDown("Jump");
         float horizontal = Input.GetAxis("Horizontal");
+        bool isAttacking = Input.GetMouseButtonDown(0);
+        
+        isGrounded = Mathf.Abs(_rigidbody.linearVelocity.y) < 0.1f;
 
         transform.Translate(horizontal * Time.deltaTime * moveSpeed, 0, 0);
-        // if (isJumping) _rigidbody.AddForce(Vector2.up * jumpForce);
         
         _animator.SetFloat(Horizontal, Mathf.Abs(horizontal));
+        _animator.SetBool(Jump, isJumping);
         
         if (horizontal > 0)
             _spriteRenderer.flipX = false;
         if (horizontal < 0)
             _spriteRenderer.flipX = true;
+        
+        _animator.SetBool("Attack", Input.GetButtonDown("Fire1"));
         
         //joueur ne vole pas avec plusieurs sauts
         if (isJumping && _rigidbody.linearVelocity.y == 0 )
@@ -39,4 +49,5 @@ public class PlayerController : MonoBehaviour
             _rigidbody.AddForce(Vector2.up * jumpForce);
         }
     }
+    
 }
